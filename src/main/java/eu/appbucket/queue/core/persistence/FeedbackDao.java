26f3d3777d719ef84e1a7427541170c9d4f73de7
Adam Bednarski <adam.bednarski@globoforce.com>
2015-07-08 12:33:03 +0100

    esitmator filters added

--- src/main/java/eu/appbucket/queue/core/persistence/FeedbackDao.java
+++ src/main/java/eu/appbucket/queue/core/persistence/FeedbackDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import eu.appbucket.queue.core.domain.feedback.FeedbackRecord;
 import eu.appbucket.queue.core.domain.feedback.FeedbackRecordFilter;
 
-public interface FeedbackDao {
+public interface  FeedbackDao {
 	void persistFeedbackRecord(FeedbackRecord record);
 	List<FeedbackRecord> findFeedbackRecords(FeedbackRecordFilter filter);
 }
